26dfa7f7b1ba45148cbc81d54ab773912c7f9ce2
Dan LaManna <dan.lamanna@kitware.com>
2016-09-12 10:15:41 -0400

    repeat-until-pass 5 times

--- girder/vagrant.sh
+++ girder/vagrant.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+export PATH="/home/dan/bin:$PATH"
+
 DIR=$(mktemp -d)
 
 function cleanup_vagrant {
@@ -13,7 +15,7 @@ git clone -b ansible-role-refactor git@github.com:girder/girder.git .
 
 # Run on girder master
 mkdir build && pushd build
-SOURCE_DIR="$DIR" ctest -S /home/dan/builds/girder/vagrant.cmake -VV
+SOURCE_DIR="$DIR" /home/dan/tmp/cmake/bin/ctest --repeat-until-pass 5 -S /home/dan/builds/girder/vagrant.cmake -VV
 
 # Run on latest girder tag
 # git checkout "$(git describe --tags $(git rev-list --tags --max-count=1))"
