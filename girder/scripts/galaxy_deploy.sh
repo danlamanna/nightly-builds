103f327cb4070b9426d0c523068c9dc8bb3fac06
Dan LaManna <dan.lamanna@kitware.com>
2016-09-15 13:30:03 -0400

    Update galaxy deploy script

--- girder/scripts/galaxy_deploy.sh
+++ girder/scripts/galaxy_deploy.sh
@@ -2,9 +2,9 @@
 set -e
 
 DIR=$(mktemp -d)
-DEPLOYED_FILE="deployed.txt"
+DEPLOYED_FILE="$(pwd)/deployed.txt"
 SUBTREE_PREFIX="devops/ansible/roles/girder"
-SUBTREE_DEST_REPO="git@github.com/girder/ansible-role-girder.git"
+SUBTREE_DEST_REPO="git@github.com:girder/ansible-role-girder.git"
 SUBTREE_DEST_BRANCH="master"
 
 pushd "$DIR"
@@ -12,6 +12,7 @@ git clone git@github.com:girder/girder.git .
 
 if  [ -f "$DEPLOYED_FILE" ] && grep -q "$(git rev-parse HEAD)" "$DEPLOYED_FILE"; then
     # This revision has already been pushed to ansible-role-girder
+    echo "Already deployed $(git rev-parse HEAD) to ansible-role-girder."
     exit 0
 else
     git subtree push --prefix="$SUBTREE_PREFIX" "$SUBTREE_DEST_REPO" "$SUBTREE_DEST_BRANCH"
